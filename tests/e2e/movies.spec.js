f5066d41f2f0973ab10fa87a40e5da0546581219
Samuel Leite <samuel-leite@outlook.com.br>
2024-03-29 12:00:00 -0300

    inclusao cenario consulta

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -4,34 +4,41 @@ const data = require("../support/fixtures/movies.json");
 
 const { executeSQL } = require("../support/dataBase");
 
-test.beforeAll(async ()=>{
+test.beforeAll(async () => {
   await executeSQL(`DELETE FROM movies`);
-})
+});
 
 test("Validar cadastro de um novo filme", async ({ page }) => {
   const movie = data.create;
 
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
   await page.movies.create(movie);
-  await page.popup.haveText(`O filme '${movie.title}' foi adicionado ao catálogo.`);
+  await page.popup.haveText(
+    `O filme '${movie.title}' foi adicionado ao catálogo.`
+  );
 });
 
 test("Validar exclusão de um filme", async ({ page, request }) => {
   const movie = data.to_remove;
-  await request.api.postMovie(movie)
+  await request.api.postMovie(movie);
 
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
-  await page.movies.remove(movie.title)
-  await page.popup.haveText('Filme removido com sucesso.');  
+  await page.movies.remove(movie.title);
+  await page.popup.haveText("Filme removido com sucesso.");
 });
 
-test("Validar cadastro de duplicidade de título do filme", async ({ page, request }) => {
+test("Validar cadastro de duplicidade de título do filme", async ({
+  page,
+  request,
+}) => {
   const movie = data.duplicate;
-  await request.api.postMovie(movie)
-  
+  await request.api.postMovie(movie);
+
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
   await page.movies.create(movie);
-  await page.popup.haveText(`O título '${movie.title}' já consta em nosso catálogo. Por favor, verifique se há necessidade de atualizações ou correções para este item.`);
+  await page.popup.haveText(
+    `O título '${movie.title}' já consta em nosso catálogo. Por favor, verifique se há necessidade de atualizações ou correções para este item.`
+  );
 });
 
 test("Validar obrigatoriedade ao cadastrar filme", async ({ page }) => {
@@ -43,6 +50,18 @@ test("Validar obrigatoriedade ao cadastrar filme", async ({ page }) => {
     "Campo obrigatório",
     "Campo obrigatório",
     "Campo obrigatório",
-    "Campo obrigatório"
+    "Campo obrigatório",
   ]);
+});
+
+test("Validar consulta pelo termo Zumbi", async ({ page, request }) => {
+  const films = data.search;
+
+  films.data.forEach(async (m) => {
+    await request.api.postMovie(m);
+  })
+
+  await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
+  await page.movies.search(films.input)
+  await page.movies.tableHave(films.outputs)
 });
